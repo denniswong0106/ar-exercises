668fda863db73241645e58824885f5a35d0c08a2
Dennis Wong <denniswong0106@gmail.com>
2021-04-28 06:44:38 +0000

    added logic for exercise 3 and 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,3 +1,4 @@
+require 'pp'
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -7,3 +8,32 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+  )
+
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "The store name is #{store.name} and the store rev is #{store.annual_revenue}"
+end
+
+pp @women_stores = Store.where(["womens_apparel = ? and annual_revenue > ?", true, 1000000])
