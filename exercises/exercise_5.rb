3c768a34badc01e76a44d4327a4e3c1583f8334d
Dennis Wong <denniswong0106@gmail.com>
2021-04-28 06:51:55 +0000

    added logic for exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+p Store.sum(:annual_revenue)
+pp Store.average(:annual_revenue).to_s
+
+pp @num_stores_over_1m = Store.where(["annual_revenue > ?", 1000000]).count
